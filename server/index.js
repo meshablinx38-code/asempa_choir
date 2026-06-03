adee997ef49fc4410a875c539a330e6d1682aed7
meshablinx38-code <meshablinx38@gmail.com>
2026-06-03 10:46:40 +0000

    add crypto import

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const axios = require("axios");
+const crypto = require("node:crypto");
+const express = require("express");const axios = require("axios");
 const cors = require("cors");
 const admin = require("firebase-admin");
 
